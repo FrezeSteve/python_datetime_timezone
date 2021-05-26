ca5814532370ddbba40e7804a68ef5139ebe4a02
Steve <32013689+FrezeSteve@users.noreply.github.com>
2021-05-26 15:16:53 +0300

    Update main.py
    This is how you store datetime in utcnow and display that in your required timezone.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import datetime
+import pytz
 
+# store this in the database or something
+_now = datetime.datetime.utcnow()
+
+# you can view the timezone which None 
+print(_now.tzinfo)
+
+# when displaying the datetime use the pytz module
+localized_datetime = pytz.utc.localize(_now)
+
+# right now the tzinfo is UTC
+print(localized_datetime.tzinfo)
+
+# then apply the timezone you want
+timezone_datetime = localized_datetime.astimezone(pytz.timezone('Africa/Nairobi'))
+print(timezone_datetime)
